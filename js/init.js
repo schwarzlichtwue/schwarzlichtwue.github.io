0acfa4df0772fca657019a8f54ade481e77c7de2
schwarzlichtwue <schwarzlicht.wuerzburg@gmail.com>
2020-01-09 21:54:04 +0000

    new tweets

--- js/init.js
+++ js/init.js
@@ -1,4 +1,6 @@
 document.addEventListener('DOMContentLoaded', function() {
-	 var elems = document.querySelectorAll('.materialboxed');
-	 var instances = M.Materialbox.init(elems, {});
+	var boxed = document.querySelectorAll('.materialboxed');
+	var instances = M.Materialbox.init(boxed, {});
+	var collapse = document.querySelectorAll('.collapsible');
+	var instances = M.Collapsible.init(collapse, {accordion: false});
  });
