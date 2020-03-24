092c1aaf99a4a67eaf6445df66cd7b158c774bf9
schwarzlicht-wue <schwarzlicht.wuerzburg@gmail.com>
2020-03-24 23:42:42 +0100

    linksunten location support

--- js/init.js
+++ js/init.js
@@ -1,10 +1,19 @@
+var sidenavinstances;
+var boxedinstances;
+var collapseinstances;
+var dropdowninstances;
+
 document.addEventListener('DOMContentLoaded', function() {
 	var elems = document.querySelectorAll('.sidenav');
-	var instances = M.Sidenav.init(elems, {});
+	sidenavinstances = M.Sidenav.init(elems, {});
 	var boxed = document.querySelectorAll('.materialboxed');
-	var instances = M.Materialbox.init(boxed, {});
+	boxedinstances = M.Materialbox.init(boxed, {});
 	var collapse = document.querySelectorAll('.collapsible');
-	var instances = M.Collapsible.init(collapse, {accordion: false});
+	collapseinstances = M.Collapsible.init(collapse, {accordion: false});
 	var dropdown = document.querySelectorAll('.dropdown-trigger');
-	var instances = M.Dropdown.init(dropdown, {hover: true});
+	dropdowninstances = M.Dropdown.init(dropdown, {hover: true});
+
+
+	let materializeLoadedEvent = new CustomEvent("materializeLoaded");
+	document.dispatchEvent(materializeLoadedEvent);
  });
